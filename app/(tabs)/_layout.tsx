e2d61b445745a1e14dc8cd408471fc40ea451deb
Rongbin Gu <81097533+Rongbin99@users.noreply.github.com>
2025-05-07 06:04:21 -0400

    Complete Setup for Web Development (#2)
    This PR prepares this branch for the development of the web aspect of
    this React Native project. Containing only web related code, development
    of GoMommy can commence with code changes being cherry-picked into
    `main` branch later after verification stage of the MVP.
    
    TODO:
    - Implement backend logic for when input is sent in the search box
    - Add prompt options
    - Work on Profile development
    - Implement backend logic for Trip Planner AI Agent
    
    LATER:
    - Cherry pick and migrate changes to mobile main branch
    - Optimize UI/UX icons and color palette

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,63 @@
+// Library imports
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+
+// Component imports
+import { HapticTab } from '@/components/HapticTab';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import TabBarBackground from '@/components/ui/TabBarBackground';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+export default function TabLayout() {
+    const colorScheme = useColorScheme();
+
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+                headerShown: false,
+                tabBarButton: HapticTab,
+                tabBarLabelPosition: 'beside-icon',
+                tabBarBackground: TabBarBackground,
+                tabBarStyle: Platform.select({
+                    ios: {
+                        // Use a transparent background on iOS to show the blur effect
+                        position: 'absolute',
+                        height: 80,
+                    },
+                    default: {
+                        height: 80,
+                    },
+                }),
+                tabBarLabelStyle: {
+                    fontSize: 16,
+                    fontWeight: '600',
+                    marginLeft: 16,
+                },
+            }}>
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Map',
+                    tabBarIcon: ({ color }) => <MaterialCommunityIcons size={44} name="map-search" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    tabBarIcon: ({ color }) => <MaterialCommunityIcons size={44} name="account-circle" color={color} />,
+                }}
+            />
+            <Tabs.Screen
+                name="temp"
+                options={{
+                    title: 'temp123',
+                    tabBarIcon: ({ color }) => <MaterialCommunityIcons size={44} name="microsoft-visual-studio-code" color={color} />,
+                }}
+            />
+        </Tabs>
+    );
+}
