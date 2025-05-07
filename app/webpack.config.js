c129ccb42472bb9a14d78d968836335e9e01cf13
Rongbin99 <rongbingu999@gmail.com>
2025-05-07 04:03:45 -0400

    fix dependencies and get maps showing

---
+++ app/webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+// from the @teovilla/react-native-web-maps docs
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.resolve.alias['react-native-maps'] = '@teovilla/react-native-web-maps';
+
+  return config;
+}; 
